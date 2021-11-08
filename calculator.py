b9f84e08bb9ea2b47594c5161595a6e084bdfbfb
Dinasree <80672865+Dinasree@users.noreply.github.com>
2021-11-09 00:04:03 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,10 +1,8 @@
 # Python program for simple calculator
-  
-# Function to add two numbers 
+  # Function to add two numbers 
 def add(num1, num2):
     return num1 + num2
-  
-# Function to subtract two numbers 
+  # Function to subtract two numbers 
 def subtract(num1, num2):
     return num1 - num2
   
